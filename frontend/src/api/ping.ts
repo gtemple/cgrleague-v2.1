b6879a9fd62f2a5cfd7634d25cfe6f048f11d6a6
Giordano Temple <giordanotemple@gmail.com>
2025-09-09 10:37:01 -0400

    update config

--- frontend/src/api/ping.ts
+++
@@ -1,5 +0,0 @@
-import { http } from "./client";
-export type PingResponse = { status: string };
-export function getPing() {
-  return http<PingResponse>("/api/ping/");
-}
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+//@ts-expect-error no types
 import eslint from 'vite-plugin-eslint'
 
 export default defineConfig({
